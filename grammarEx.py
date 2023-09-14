87b8b2b587f01884d688b77a93de366ee3087d34
Kimhyunjin4455 <dp4455@naver.com>
2023-09-14 18:13:15 +0900

    0914PyEx0

--- grammarEx.py
+++ grammarEx.py
@@ -4,6 +4,9 @@ from itertools import combinations
 from itertools import product
 from itertools import combinations_with_replacement
 import heapq
+from bisect import bisect_left, bisect_right
+from collections import deque
+from collections import Counter
 
 #1
 a = 0.3 + 0.6
@@ -191,6 +194,34 @@ def heapsort2(iterable):
 result_20_1 = heapsort2([1,3,5,7,9,2,4,6,8,0])
 print(result_20_1)
 
-#21 bisect
+#21 bisect o(logn)
+#정렬된 배열에서 특정한 원소를 찾을 때 효과적
+#bisect_left: 정렬된 배열을 유지하면서 리스트a에 삽입할 데이터x를 삽입할 가장 왼쪽 인덱스를 찾음
+a = [1,2,4,4,8]
+x = 4
+print(bisect_left(a,x)) #2
+print(bisect_right(a,x)) #4
+
+def count_by_range(a, left_value, right_value):
+    right_index = bisect_right(a, right_value)
+    left_index = bisect_left(a, left_value)
+    return right_index - left_index
+
+a_21 = [1,2,3,3,3,3,4,4,8,9]
+print(count_by_range(a_21,4,4)) # 값이 4인 데이터 개수 출력, 2개
+print(count_by_range(a_21,-1,3)) # 값이 -1~3 인 데이터 개수 출력, 6개
+
+#22
+data_22 = deque([2,3,4])
+data_22.appendleft(1)
+data_22.append(5)
+
+print(data_22)
+print(list(data_22))
+
+counter_22 = Counter(['r', 'b', 'r', 'g', 'b', 'b'])
+print(counter_22['b']) # 'b'가 등장한 횟수 출력
+print(counter_22['g'])
+print(dict(counter_22)) # 사전 자료형으로 변환시킴
 
 
