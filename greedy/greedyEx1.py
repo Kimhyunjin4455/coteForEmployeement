1c75c4ebd180c98bd045839abb363fca813eba6d
Kimhyunjin4455 <dp4455@naver.com>
2023-09-15 16:19:22 +0900

    0915PyGreedy1

---
+++ greedy/greedyEx1.py
@@ -0,0 +1,40 @@
+#1
+# n,m,k = map(int, input().split())
+# data = list(map(int, input().split()))
+#
+# data.sort()
+# first = data[n-1]
+# second = data[n-2]
+#
+# result = 0
+#
+# while True:
+#     for i in range(k):
+#         if m == 0:
+#             break
+#         result += first
+#         m -= 1
+#     if m == 0:
+#         break
+#     result += second
+#     m -= 1
+#
+# print(result)
+
+#2
+n,m,k = map(int, input().split())
+data = list(map(int, input().split()))
+
+data.sort()
+first = data[n-1]
+second = data[n-2]
+
+# 가장 큰수가 더해지는 횟수(사용 횟수)
+cnt = int(m/(k+1)) * k
+cnt += m % (k+1)
+
+result = 0
+result += (cnt) * first
+result += (m-cnt) * second
+
+print(result)
