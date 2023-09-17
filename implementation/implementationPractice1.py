2fcd0cb7370927bf88cf06e1beda809ef020f597
Kimhyunjin4455 <dp4455@naver.com>
2023-09-18 00:09:38 +0900

    0918PyImplePrac1

--- implementation/implementationPractice1.py
+++ implementation/implementationPractice1.py
@@ -1,1 +1,18 @@
 n = int(input())
+x, y =1, 1
+move_list = input().split()
+
+dx = [0,0,-1,1]
+dy = [-1,1,0,0]
+move_types = ['L', 'R', 'U', 'D']
+
+for move in move_list:
+    for i in range(len(move_types)):
+        if move == move_types[i]:
+            nx = x + dx[i]
+            ny = y + dy[i]
+    if nx < 1 or ny < 1 or nx > n or ny > n:
+        continue
+    x, y = nx, ny
+
+print(x, y)
