53277013f7224c3c9e740a253583d47856d3fbf0
Kimhyunjin4455 <dp4455@naver.com>
2023-09-19 16:56:50 +0900

    0919PyDfsPrac

---
+++ DfsBfs/dfsPrac1.py
@@ -0,0 +1,23 @@
+def dfs(graph, v, visited):
+    visited[v] = True
+    print(v, end=' ')
+    for i in graph[v]:
+        if not visited[i]: # True가 아니면
+            dfs(graph, i, visited)
+
+# 각 노드가 연결된 정보를 리스트 자료형으로 표현
+# 1>2, 2>7, 7>6 7>8순서로 갔다가 1>3 3>4 4>5
+graph = [
+    [],
+    [2,3,8],
+    [1,7],
+    [1,4,5],
+    [3,5],
+    [3,4],
+    [7],
+    [2,6,8],
+    [1,7]
+]
+
+visited = [False]*9
+dfs(graph, 1, visited)
