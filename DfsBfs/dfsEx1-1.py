97dd3e88742273b9931f8dc78a3dac5d90a4419c
Kimhyunjin4455 <dp4455@naver.com>
2023-09-20 14:26:07 +0900

    0920PySortPrac

--- DfsBfs/dfsEx1-1.py
+++ DfsBfs/dfsEx1-1.py
@@ -7,11 +7,12 @@ for i in range(n):
 def dfs(x,y):
     if x <= -1 or x >= n or y <= -1 or y >= m:
         return False
+
     if graph[x][y] == 0:
         graph[x][y] = 1
         dfs(x-1, y)
-        dfs(x, y-1)
         dfs(x+1, y)
+        dfs(x, y-1)
         dfs(x, y+1)
         return True
     return False
@@ -19,7 +20,7 @@ def dfs(x,y):
 result = 0
 for i in range(n):
     for j in range(m):
-        if dfs(1,j) == True:
-            result += 1
+        if dfs(i, j) == True:
+            result +=1
 
 print(result)
