9be06b24178bd14cdd181df3bd5116c09daaf2e7
Kimhyunjin4455 <dp4455@naver.com>
2023-10-16 22:06:30 +0900

    1016sortReview

--- sort/sortEx1.py
+++ sort/sortEx1.py
@@ -1,12 +1,22 @@
+# n = int(input())
+# answer = []
+# for _ in range(n):
+#     answer.append(int(input()))
+#
+# # answer.sort(reverse=True)
+# # for i in range(len(answer)):
+# #     print(answer[i], end=' ')
+#
+# answer = sorted(answer, reverse=True)
+# for i in answer:
+#     print(i, end=' ')
+
 n = int(input())
 answer = []
 for _ in range(n):
     answer.append(int(input()))
-    
-# answer.sort(reverse=True)
-# for i in range(len(answer)):
-#     print(answer[i], end=' ')
 
-answer = sorted(answer, reverse=True)
+answer.sort(reverse=True)
+
 for i in answer:
-    print(i, end=' ')
+    print(i, end=" ")
--- sort/sortEx3.py
+++ sort/sortEx3.py
@@ -1,14 +1,30 @@
-n,k = map(int, input().split())
-a_list = list(map(int, input().split()))
-b_list = list(map(int, input().split()))
+# n,k = map(int, input().split())
+# a_list = list(map(int, input().split()))
+# b_list = list(map(int, input().split()))
+#
+# a_list.sort()
+# b_list.sort(reverse=True)
+#
+# for i in range(k):
+#     if(a_list[i] < b_list[i]):
+#         a_list[i] = b_list[i]
+#     else:
+#         break
+#
+# print(sum(a_list))
 
-a_list.sort()
-b_list.sort(reverse=True)
+
+n,k = map(int, input().split())
+array_a = list(map(int, input().split()))
+array_b = list(map(int, input().split()))
+
+array_a.sort()
+array_b.sort(reverse=True)
 
 for i in range(k):
-    if(a_list[i] < b_list[i]):
-        a_list[i] = b_list[i]
+    if array_a[i] < array_b[i]:
+        array_a[i], array_b[i] = array_b[i], array_a[i]
     else:
-        break
-        
-print(sum(a_list))
+        break # 오름차순 정렬의 값이 내림차순 정렬의 값보다 크다면 그 이후는 계산 X
+
+print(sum(array_a))
