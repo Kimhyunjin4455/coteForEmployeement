20dbf49dc5681a16529a57f963ddf6662230082c
Kimhyunjin4455 <dp4455@naver.com>
2023-10-17 17:01:26 +0900

    1017bsEx2AddAnnotation

--- DfsBfs/bfsExRe.py
+++ DfsBfs/bfsExRe.py
@@ -1,36 +1,80 @@
+# from collections import deque
+#
+# n,m = map(int, input().split())
+# graph = []
+#
+# for _ in range(n):
+#     graph.append(list(map(int, input())))
+#
+# dx = [-1, 1, 0, 0]
+# dy = [0,0,-1,1]
+#
+# def bfs(x,y):
+#
+#     queue = deque()
+#     queue.append((x,y))
+#
+#     while queue:
+#         x, y = queue.popleft()
+#
+#         for i in range(4):
+#             nx = x + dx[i]
+#             ny = y + dy[i]
+#
+#             if nx <0 or nx >=n or ny <0 or ny >= m: # 미로 공간을 벗어난 경우
+#                 continue
+#             if graph[nx][ny] == 0: # 미로의 벽을 만난 경우
+#                 continue
+#
+#             if graph[nx][ny] == 1:
+#                 graph[nx][ny] = graph[x][y]+1
+#                 queue.append((nx,ny))
+#
+#     return graph[n-1][m-1]
+#
+#
+# print(bfs(0,0))
+
 from collections import deque
 
 n,m = map(int, input().split())
-graph = []
-
+maze = []
 for _ in range(n):
-    graph.append(list(map(int, input())))
+    maze.append(list(map(int, input())))
 
-dx = [-1, 1, 0, 0]
+
+visited = [[0]*m for _ in range(n)]
+time = [[0]*m for _ in range(n)]
+
+visited[0][0] = 1
+time[0][0] = 1
+
+dx = [-1,1,0,0]
 dy = [0,0,-1,1]
 
 def bfs(x,y):
-
     queue = deque()
     queue.append((x,y))
-
     while queue:
-        x, y = queue.popleft()
-
+        x, y = queue.popleft() # for문 안에서 시도하면 queue가 빈채로 popleft가 진행될수도 있음
         for i in range(4):
+
             nx = x + dx[i]
             ny = y + dy[i]
 
-            if nx <0 or nx >=n or ny <0 or ny >= m: # 미로 공간을 벗어난 경우
+            if nx < 0 or nx >= n or ny < 0 or ny >= m:
                 continue
-            if graph[nx][ny] == 0: # 미로의 벽을 만난 경우
+            if maze[nx][ny] == 0:
                 continue
-
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y]+1
+            if maze[nx][ny] == 1 and visited[nx][ny] == 0:
                 queue.append((nx,ny))
+                visited[nx][ny] = 1
+                time[nx][ny] = time[x][y] + 1
+                continue
 
-    return graph[n-1][m-1]
-
+    return time[n-1][m-1]
 
 print(bfs(0,0))
+
+
+
--- binarySearch/bsEx2Re.py
+++ binarySearch/bsEx2Re.py
@@ -1,50 +1,47 @@
-# n,m = map(int, input().split())
-# dduck_list = list(map(int, input().split()))
-#
-# start = 0
-# end = max(dduck_list)
-#
-# result = 0
-#
-# while(start < end):
-#     total = 0
-#     mid = (start + end) // 2
-#     for x in dduck_list:
-#         if x > mid:
-#             total += (x-mid)
-#     if total > m:
-#         start = mid+1
-#     else:
-#         result = mid
-#         end = mid-1
-#
-# print(result)
-
-
 n,m = map(int, input().split())
-dduck_len = list(map(int, input().split()))
+dduck_list = list(map(int, input().split()))
 
-end = max(dduck_len)
+start = 0
+end = max(dduck_list)
 
-def bs(array, target, start, end):
+result = 0
 
+while(start < end):
+    total = 0
     mid = (start + end) // 2
-    product = 0
-    for dduck in dduck_len:
-        if dduck >= mid:
-            product += (dduck-mid)
-    if product == m:
-        return mid
-    elif product > m:
-        return bs(array, target, mid+1, end)
-    else:
-        return bs(array, target, start, mid-1)
-
-print(bs(dduck_len, m, 0, end))
+    for x in dduck_list:
+        if x > mid:
+            total += (x-mid)
+    if total > m:
+        start = mid+1 #자를 길이를 더 뒤로 설정해야함
+    else: # start가 m보다 같거나 작을 경우
+        result = mid # 같을 경우를 대비해 값 저장
+        end = mid - 1 #자를 길이의 기준을 더 앞으로 설정해야 함(자른 길이가 더 많아지도록 설정)
 
 
 
+print(result)
 
 
 
-
+# n,m = map(int, input().split())
+# dduck_len = list(map(int, input().split()))
+#
+# end = max(dduck_len)
+#
+# def bs(array, target, start, end):
+#
+#     mid = (start + end) // 2
+#     product = 0 # 자른 길이의 합
+#     # 각 떡의 길이에 대해
+#     for dduck in dduck_len:
+#         if dduck >= mid: # 떡의 길이가 중간값보다 크면(자를 수 있으면)
+#             product += (dduck-mid) # 총 길이에 더함
+#     if product == m: # 길이가 딱 맞으면 중간값 반환
+#         return mid
+#     elif product > m: # 길이가 크면 기준을 뒤로 설정(뒤로 설정해야 자르는 떡의 길이의 합이 작아음)
+#         return bs(array, target, mid+1, end)
+#     else: # 길이가 짧으면 기준을 앞으로 설정
+#         return bs(array, target, start, mid-1)
+#
+# print(bs(dduck_len, m, 0, end))
