6ac5749ab58c1e85bde8bc9772b09245cd65a225
Kimhyunjin4455 <dp4455@naver.com>
2023-12-04 17:24:08 +0900

    coteEmpQ15withDef

--- Q/15.py
+++ Q/15.py
@@ -1,37 +1,33 @@
-# 1~N번 도시, M개 단방향 도로
-# 특정 도시 x로 부터 출발하여 도달할 수 있는 모든 도시 중에서 '최단 거리'가 정확히 k 인 모든 도시 번호 출력
-
-import sys
+from collections import deque
+# import sys
 # input = sys.stdin.readline()
 
-from collections import deque
-
 n,m,k,x = map(int, input().split())
-visited = [[] for _ in range(n+1)] # [[] *(n+1)] 차이 뭔지
-
-for i in range(m):
+area = [[] for _ in range(n+1)]
+distance = [-1] * (n+1)
+for _ in range(m):
     a,b = map(int, input().split())
-    visited[a].append(b)
+    area[a].append(b)
 
-distance = [[-1] * (n+1)] # 최단 거리 초기화
-distance[x] = 0
+def bfs(x):
+    queue = deque()
+    queue.append(x)
+    distance[x] = 0
 
-queue = deque()
-queue.append(x)
+    while queue:
+        now = queue.popleft()
+        for next in area[now]:
+            if distance[next] == -1:
+                distance[next] = distance[now] + 1
+                queue.append(next)
 
-cnt = 0
+    for i in range(len(distance)):
+        if distance[i] == k:
+            print(i)
+    else:
+        if k not in distance:
+            print(-1)
 
-while queue:
-    v = queue.popleft()
-    for i in visited[v]:
-        if distance[i] < distance[v] + 1:
-            distance[i] = distance[v] + 1
-            queue.append(i)
+bfs(x)
 
-for result in range (len(distance)):
-    if distance[result] == k:
-        print(result)
-else:
-    print(-1)
-print(visited)
 
