adf918d6e19aaad195203bf58113079344c845ee
Kimhyunjin4455 <dp4455@naver.com>
2023-12-12 23:47:29 +0900

    coteEmpQ21Solve

--- Q/Dfs&Bfs/21.py
+++ Q/Dfs&Bfs/21.py
@@ -1,49 +1,60 @@
+from collections import deque
+
 N,L,R = map(int, input().split())
 area = [list(map(int, input().split())) for _ in range(N)]
-visited = [[False]* N for _ in range(N)]
-peoples = 0
-result = 0
-dr = [1,-1,0,0]
+
+
+ans = 0
+union_lst = []
+dr = [-1,1,0,0]
 dc = [0,0,-1,1]
 
 
-def dfs(i, j):
-    global peoples
-    global result
-    global recur
-
-    row = i
-    col = j
-
+def bfs(i,j):
+    queue = deque()
+    queue.append((i,j))
     visited[i][j] = True
-    peoples += area[row][col]
+    union_lst = [(i, j)]
+    peoples = area[i][j]
 
-    for dir in range(4):
-        nr = row + dr[dir]
-        nc = col + dc[dir]
-        if 0 <= nr <N and 0 <= nc < N:
-            if (visited[nr][nc] == False) and L <= abs(area[row][col] - area[nr][nc]) <= R:
-                recur.append(0)
-                dfs(nr, nc)
+    while queue:
+        now_row, now_col = queue.popleft()
 
-    area[row][col] = int(peoples/len(recur))
-    visited[row][col] = False
+        for dir in range(4):
+            nr = now_row + dr[dir]
+            nc = now_col + dc[dir]
+            if 0 <= nr < N and 0 <= nc < N and (visited[nr][nc] == False):
+                if L <= abs(area[now_row][now_col] - area[nr][nc]) <= R:
+                    queue.append((nr,nc))
+                    visited[nr][nc] = True
+                    peoples += area[nr][nc]
+                    union_lst.append((nr,nc))
+
+    for (row,col) in union_lst:
+        area[row][col] = int(peoples/len(union_lst))
+
+    if len(union_lst) > 1:
+        return True
+
+    return False
 
 
 
 
+while ans <= 2000:
+    visited = [[False] * N for _ in range(N)]
+    flag = 0
+
+    for i in range(N):
+        for j in range(N):
+            if visited[i][j] == False:
+                t = bfs(i,j)     #
+                if t == True:       #
+                    flag = 1     # 위 세줄을 t = max(t, bfs(i,j)) 로 치환 가능
 
 
-for i in range(N):
-    for j in range(N):
-        new_area = area.copy()
-        if visited[i][j] == False:
-            peoples = 0
-            recur = [0]
-            dfs(i,j)
-        if area == new_area:
-            result += 1
+    if flag == 0:
+        break
+    ans += 1
 
-print(area)
-print(result)
-
+print(ans)
