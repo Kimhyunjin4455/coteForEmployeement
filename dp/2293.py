80a56f4118ce682c2018a3cdf6cbe40dc7611afc
Kimhyunjin4455 <dp4455@naver.com>
2023-12-17 23:38:49 +0900

    baekJoon2293

---
+++ dp/2293.py
@@ -0,0 +1,18 @@
+# n가지의 동전, 합이 k원이 되도록하는 경우의 수(동전은 중복 사용 가능)
+N,K = map(int, input().split())
+nums = [int(input()) for _ in range(N)]
+dp = [0] * (K+1)
+dp[0] = 1
+
+for i in range(len(nums)):
+    for j in range(nums[i], K+1): # 최대 반복수는 최대 금액때까지
+        dp[j] = dp[j] + dp[j - nums[i]]
+print(dp)
+print(dp[K])
+
+
+
+
+
+
+
