6dab628799d7328b3a601cfb6ddfcff2c8b697de
Kimhyunjin4455 <dp4455@naver.com>
2023-12-18 18:44:40 +0900

    baekJoonQ11054

---
+++ dp/11054.py
@@ -0,0 +1,25 @@
+n = int(input())
+nums = list(map(int, input().split()))
+rs_incre = [1] * (n)
+rs_decre = [1] * (n)
+
+for i in range(n):
+    for j in range(i):
+        if nums[i] > nums[j]:
+            rs_incre[i] = max(rs_incre[i], rs_incre[j]+1)
+
+nums.reverse() # 역순으로 증가구하기 == 감소하는 수열 구하기
+for i in range(n):
+    for j in range(i):
+        if nums[i] > nums[j]:
+            rs_decre[i] = max(rs_decre[i], rs_decre[j]+1)
+rs_decre.reverse()
+
+def calcul(rs_incre, rs_decre):
+    cal = [0] * n
+    for i in range(n):
+        cal[i] = rs_incre[i] + rs_decre[i]
+
+    return max(cal)
+
+print(calcul(rs_incre, rs_decre)-1)
